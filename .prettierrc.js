5d9b8da865d77803720064d020a92b29d9277162
EndureBlaze <hi@imyan.ren>
2022-02-25 11:38:23 +0800

    :bento: Update web icon

--- .prettierrc.js
+++
@@ -1,4 +0,0 @@
-exports = {
-  singleQuote: true, // 使用单引号
-  semi: false, // 句尾不使用分号
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,34 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
-import inject from "@rollup/plugin-inject";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
-      "@page": resolve(__dirname, "src/page"),
-      process: "process/browser",
-      stream: "stream-browserify",
-      zlib: "browserify-zlib",
-      util: "util",
+      '@': resolve(__dirname, 'src'),
+      '@page': resolve(__dirname, 'src/page'),
+      process: 'process/browser',
+      stream: 'stream-browserify',
+      zlib: 'browserify-zlib',
+      util: 'util',
     },
   },
-  base: "./",
+  base: './',
   server: {
     port: 3000,
   },
   define: {
-    "process.env.BASE_URL": '"/"',
+    'process.env.BASE_URL': '"/"',
   },
   build: {
     rollupOptions: {
-      plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
+      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
     },
     commonjsOptions: {
       transformMixedEsModules: false,
     },
   },
-});
+})
