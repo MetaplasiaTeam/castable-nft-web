5b967c4114d7d0387ad62e4553c41c49293c854d
EndureBlaze <hi@imyan.ren>
2022-03-15 17:25:34 +0800

    :rotating_light: remove pdfjs-dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import inject from '@rollup/plugin-inject'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    //viteCompression()
+  ],
   assetsInclude: ['src/assets/*'],
   resolve: {
     alias: {
@@ -36,17 +40,32 @@ export default defineConfig({
     rollupOptions: {
       // @ts-ignore
       plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
-      // output: {
-      //   manualChunks(id) {
-      //     if (id.includes('node_modules')) {
-      //       return id
-      //         .toString()
-      //         .split('node_modules/')[1]
-      //         .split('/')[0]
-      //         .toString()
-      //     }
-      //   },
-      // },
+      output: {
+        // manualChunks(id) {
+        //   if (id.includes('node_modules')) {
+        //     return id
+        //       .toString()
+        //       .split('node_modules/')[1]
+        //       .split('/')[0]
+        //       .toString()
+        //   }
+        // },
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (
+              id
+                .toString()
+                .split('node_modules/')[1]
+                .split('/')[0]
+                .includes('naive-ui')
+            ) {
+              return 'naive-ui'
+            } else {
+              return 'vendor'
+            }
+          }
+        },
+      },
     },
   },
 })
